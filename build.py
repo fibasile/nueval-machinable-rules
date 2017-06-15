8d39dc9260a711e99919eb5c875c3227623ea5e5
Fiore Basile <fiore.basile@gmail.com>
2017-06-15 12:44:26 +0200

    fixed syntax and updated build script

--- build.py
+++ build.py
@@ -1,12 +1,12 @@
+import sys, traceback
 import yaml
-import sys,traceback
 import os
 import json
 from cStringIO import StringIO
+import glob
+SOURCE_DIR = os.path.join(os.getcwd(), 'src')
+SOURCE_FILES = glob.glob(os.path.join(SOURCE_DIR, '*.yaml'))
 
-SOURCE_FILES = [
-    'computer-controlled_cutting'
-]
 
 def readYAML(source):
     print 'Reading %s' % source
@@ -14,30 +14,29 @@ def readYAML(source):
     data = yaml.load(f.read())
     f.close()
     return data
-    
-    
+
+
 def makeTask(task):
     taskMD = StringIO()
-    print >>taskMD, '## %s\n' % task['name']
-    print >>taskMD, task['description']
-    print >>taskMD, '### Learning outcomes\n'
+    print >> taskMD, '## %s\n' % task['name']
+    print >> taskMD, task['description']
+    print >> taskMD, '### Learning outcomes\n'
     for o in task['outcomes']:
-        print >>taskMD, '* %s' % o
-    print >>taskMD, '\n### Have you?\n'
+        print >> taskMD, '* %s' % o
+    print >> taskMD, '\n### Have you?\n'
     for c in task['checklist']:
-        print >>taskMD, '* %s' % c
+        print >> taskMD, '* %s' % c
     return taskMD.getvalue()
 
-def makeBookPage(data):
-    md =  StringIO()
-    print >>md, '# %s\n' % data['unit']
-    for task in data['tasks']:
-        print >>md, makeTask(task)
-    print >>md, '## FAQ\n'
-    print >>md, data['faq']
-    return md.getvalue()
-        
 
+def makeBookPage(data):
+    md = StringIO()
+    print >> md, '# %s\n' % data['unit']
+    for task in data['tasks']:
+        print >> md, makeTask(task)
+    print >> md, '## FAQ\n'
+    print >> md, data['faq']
+    return md.getvalue()
 
 
 if __name__ == '__main__':
@@ -49,24 +48,24 @@ if __name__ == '__main__':
         print '   gitbook: Build markdown pages from YAML source\n\n'
         sys.exit(-1)
     command = sys.argv[1]
-    
+
     for source in SOURCE_FILES:
         if command == 'test':
             try:
-                data = readYAML(os.path.join(os.getcwd(), '%s.yaml' % source))
+                data = readYAML(source)
                 print 'Syntax OK'
-            except Exception:
+            except Exception, ex:
                 print 'Syntax Error'
+                print ex
         elif command == 'json':
             try:
-                data = readYAML(os.path.join(os.getcwd(), '%s.yaml' % source))
-                
+                data = readYAML(source)
                 print json.dumps(data, indent=4)
             except Exception:
                 print 'Syntax Error'
         elif command == 'gitbook':
             try:
-                data = readYAML(os.path.join(os.getcwd(), '%s.yaml' % source))
+                data = readYAML(source)
                 print makeBookPage(data)
-            except Exception,ex:
-                 traceback.print_exc()
+            except Exception, ex:
+                traceback.print_exc()
